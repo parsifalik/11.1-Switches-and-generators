9e37ccd1c9af55540d9759b14f47e54b52942b1f
parsifalik <super.tebyakin68@yandex.ru>
2024-06-25 23:31:13 +0300

    Update задача.py

--- Функции-генераторы (yield)/задача.py
+++ Функции-генераторы (yield)/задача.py
@@ -15,3 +15,29 @@ def inf_seq(num=1):
 number = inf_seq()
 print(next(number))
 print(next(number))
+
+#=====================================
+from typing import List, Dict, Any, Generator
+
+def filter_by_currency(transactions: List[Dict[str, Any]], currency_code: str) -> Generator[Dict[str, Any], None, None]:
+    for transaction in transactions:
+        if transaction.get('currency_code') == currency_code:
+            yield transaction
+
+
+transactions = [
+    {'id': 1, 'currency_code': 'USD', 'amount': 100},
+    {'id': 2, 'currency_code': 'EUR', 'amount': 200},
+    {'id': 3, 'currency_code': 'USD', 'amount': 150},
+]
+
+filtered_transactions = filter_by_currency(transactions, 'USD')
+
+# Используем генератор в цикле for
+for transaction in filtered_transactions:
+    print(transaction)
+
+
+# {'id': 1, 'currency_code': 'USD', 'amount': 100}
+# {'id': 3, 'currency_code': 'USD', 'amount': 150}
+
